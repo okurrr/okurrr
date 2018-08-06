0306b93b8dd831fdf647cdde0c006108668b8fc1
Lawrence Lin <lawrence.y.lin@gmail.com>
2018-08-05 23:47:31 -0400

    test3

--- client/components/home.spec.js
+++ client/components/home.spec.js
@@ -4,7 +4,7 @@ import {expect} from 'chai'
 import React from 'react'
 import enzyme, {shallow} from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
-import {NavBar} from './navbar'
+import {Navbar} from './navbar'
 
 const adapter = new Adapter()
 enzyme.configure({adapter})
